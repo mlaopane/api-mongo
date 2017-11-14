2f8abb8e4c831bbc5da77cc33dab0a9463369491
mlaopane <mlaopane@gmail.com>
2017-11-14 19:55:51 +0100

    Added : App::loadContainer for Functional tests

--- src/App.php
+++ src/App.php
@@ -3,27 +3,35 @@ namespace MykeOn;
 
 use Psr\Container\ContainerInterface;
 
+define('ROOT_DIR', dirname(__DIR__));
+define('CONFIG_DIR', ROOT_DIR.'/app/config');
+
 /**
  *
  */
 class App
 {
+    private static $container;
+
     /**
      * Entry point for the API
      */
     public static function launch()
     {
-        define('ROOT_DIR', dirname(__DIR__));
-        define('CONFIG_DIR', ROOT_DIR.'/app/config');
-
         $app = new \Slim\App(["settings" => require CONFIG_DIR.'/settings.php']);
 
         session_start();
 
         require CONFIG_DIR.'/container.php';
-
         require CONFIG_DIR.'/routes.php';
 
         $app->run();
+    }
+
+    public static function loadContainer()
+    {
+        $app = new \Slim\App(["settings" => require CONFIG_DIR.'/settings.php']);
+        require CONFIG_DIR.'/container.php';
+        return $container;
     }
 }
