bb10d846649242801d40a590d7d430b294cdf380
mlaopane <mlaopane@gmail.com>
2017-11-17 19:53:09 +0100

    Added + Updated : routes for search, replace, patch + middlewares

--- app/config/routes.php
+++ app/config/routes.php
@@ -3,43 +3,48 @@
 use MykeOn\Controller\Http\{
     GetController,
     PostController,
-    PutController,
+    SearchController,
+    ReplaceController,
+    PatchController,
     DeleteController
 };
 use MykeOn\Middleware\{
-    AccessControlMiddleware,
-    CacheMiddleware
+    HeadersMiddleware,
+    CacheRequestMiddleware
 };
 
-$app->add(new AccessControlMiddleware());
+/* -------------------- */
 
-/** GET request */
 $app
-    ->get("/{database:[a-z0-9]+}[/{collection:[a-z0-9]+}[/{id:[a-z0-9]+}]]", GetController::class.":handleRequest")
-    ->setName('get')
-    ->add(new CacheMiddleware($container['cache'], 'request'));
+->add(new CacheRequestMiddleware($container['cache']))
+->add(new HeadersMiddleware())
+;
 
-/** ACTION collection request */
-$app
-    ->post("/{database:[a-z0-9]+}/{collection:[a-z0-9]+}/{action:_[a-z]+}", PostController::class.":handleRequest")
-    ->setName('action_collection');
+/** Requests uri MUST provide the database **/
+$app->group("/{database:[a-z0-9]+}", function () {
 
-/** ACTION database request */
-$app
-    ->post("/{database:[a-z0-9]+}/{action:_[a-z]+}", PostController::class.":handleRequest")
-    ->setName('action_database');
+    /** READ **/
+    $this->get("[/{collection:[a-z0-9]+}[/{id:[a-z0-9]+}]]", GetController::class.":handleRequest")->setName('get');
 
-/** POST request */
-$app
-    ->post("/{database:[a-z0-9]+}/{collection:[a-z0-9]+}", PostController::class.":handleRequest")
-    ->setName('post');
+    /** CREATE **/
+    $this->post("/{collection:[a-z0-9]+}", PostController::class.":handleRequest")->setName('search_database');
 
-/** PUT request */
-$app
-    ->put("/{database:[a-z0-9]+}/{collection:[a-z0-9]+}/{id:[a-z0-9]+}", PutController::class.":handleRequest")
-    ->setName('put');
+    /** SEARCH database **/
+    $this->post("/_search", SearchController::class.":handleRequest")->setName('search_database');
 
-/** DELETE request */
-$app
-    ->delete("/{database:[a-z0-9]+}[/{collection:[a-z0-9]+}[/{id:[a-z0-9]+}]]", DeleteController::class.":handleRequest")
-    ->setName('delete_request');
+    /** SEARCH collection **/
+    $this->post("/{collection:[a-z0-9]+}/_search", SearchController::class.":handleRequest")->setName('search_collection');
+
+    /** REPLACE **/
+    $this->put("/{collection:[a-z0-9]+}/{id:[a-z0-9]+}", ReplaceController::class.":handleRequest")->setName('put');
+
+    /** UPDATE collection **/
+    $this->patch("/{collection:[a-z0-9]+}/_{action:[a-z0-9]+}", PatchController::class.":handleRequest")->setName('patch');
+
+    /** UPDATE document **/
+    $this->patch("/{collection:[a-z0-9]+}/{id:[a-z0-9]+}/_{action:[a-z0-9]+}", PatchController::class.":handleRequest")->setName('patch');
+
+    /** DELETE request */
+    $this->delete("[/{collection:[a-z0-9]+}[/{id:[a-z0-9]+}]]", DeleteController::class.":handleRequest")->setName('delete');
+
+});
